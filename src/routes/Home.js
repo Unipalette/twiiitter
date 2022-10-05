999c632d680f0c45736ba6dd1fbcdf2bde468cb0
Unipalette <wjdtmddbs16@naver.com>
2022-10-05 21:35:19 +0900

    Home Change

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 function Home() {
+  const [tweeet, setTweet] = useState("");
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setTweet(value);
+  };
+  console.log(tweeet);
   return (
     <div>
-      <form>
-        <input type="text" placeholder="What's on your mind?" maxLength={120} />
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={tweeet}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
         <input type="submit" value="tweeet"></input>
       </form>
     </div>
